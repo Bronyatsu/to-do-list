b1b06dffc70bb56e70aeccd267fe5798a286e639
David <fankmen@gmail.com>
2024-06-04 17:18:10 +0200

    first big fat commit

---
+++ todo_app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from .models import Owner, Patient
+from .forms import OwnerCreateForm, PatientCreateForm
+
+# Create your views here.
